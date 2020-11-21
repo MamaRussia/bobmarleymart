1207ba45bea4b3e4e676b92ad42e9bae07c91d82
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2020-11-20 18:06:21 -0700

    added + tested  mysql connection

--- bmm-customer/bmmCustomer.js
+++ bmm-customer/bmmCustomer.js
@@ -1,1 +1,15 @@
- 
+let mysql = require("mysql");
+let connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("There is an error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("Connected as ID: " + connection.threadId);
+});
