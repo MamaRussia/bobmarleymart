d1f1c9cd918e1d59cccfdbb53d889f822be717b0
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2021-01-09 12:28:30 -0700

    gitignore and eslint added

--- bmm-customer/bmmCustomer.js
+++ bmm-customer/bmmCustomer.js
@@ -1,59 +1,56 @@
-const mysql = require("mysql");
-const inquirer = require("inquirer");
-const { table } = require("console");
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-    password: "password",
-  database: "bobmarleymart"
-});
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const { table } = require('console');
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'bobmarleymart',
+});
 
 connection.connect(function (err) {
   if (err) {
-    console.error("There is an error connecting: " + err.stack);
+    console.error(`There is an error connecting: ${err.stack}`);
     return;
   }
 
-    console.log("Connected as ID: " + connection.threadId);
-    
-    loadProducts();
+  console.log(`Connected as ID: ${connection.threadId}`);
 
+  loadProducts();
 });
 
 function loadProducts() {
-    
-    //select all products from SQL table
-    connection.query("SELECT * FROM products", function (err, res) {
-        
-        if (err) throw err;
+  // select all products from SQL table
+  connection.query('SELECT * FROM products', function (err, res) {
+    if (err) throw err;
 
-        console.table(res);   
-    })
+    console.table(res);
+  });
 }
 
 inquirer
-    .prompt([{
-        name: "strainToBuy",
-        message: "What is the ID of the strain you want to buy? [Quit with Q]" ,
-      
-    },   /* Pass your questions in here */
+  .prompt([
+    {
+      name: 'strainToBuy',
+      message: 'What is the ID of the strain you want to buy? [Quit with Q]',
+    } /* Pass your questions in here */,
   ])
-    .then(answers => {
-      console.log("Answer:", answers.strainToBuy);
+  .then((answers) => {
+    console.log('Answer:', answers.strainToBuy);
     // Use user feedback for... whatever!!
   })
-  .catch(error => {
-    if(error.isTtyError) {
+  .catch((error) => {
+    if (error.isTtyError) {
       // Prompt couldn't be rendered in the current environment
     } else {
       // Something else when wrong
     }
   });
 
-  function checkIfIShouldExit(choice) {
-      if (choice.toLowerCase() === "q") {
-          console.log("Goodbye!");
-          process.exit(0);
-      }
+function checkIfIShouldExit(choice) {
+  if (choice.toLowerCase() === 'q') {
+    console.log('Goodbye!');
+    process.exit(0);
   }
+}
